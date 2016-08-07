401945a146eb611700a6050fa7d09eb2cf624a37
Adam <zealan@gmail.com>
2016-08-06 22:38:55 -0400

    replace custom offset method with BlockPos.offset

--- java/us/thinkplank/goldinthemtharhills/ItemGoldPan.java
+++ java/us/thinkplank/goldinthemtharhills/ItemGoldPan.java
@@ -30,47 +30,26 @@ public class ItemGoldPan extends Item {
 	
     @Override
     public EnumActionResult onItemUse(ItemStack itemStack, EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-    	if (itemStack == null) {
-    		return EnumActionResult.FAIL;
-    	} else {
-			Block block = world.getBlockState(wheresTheWater(pos, facing)).getBlock();
+		Block block = world.getBlockState(pos.offset(facing)).getBlock();
+		
+		System.out.println("THE BLOCK IS " + block.toString());
+		if (block.equals(Blocks.WATER)|| block.equals(Blocks.FLOWING_WATER)) {
+			double chance = probability;
 			
-			System.out.println("THE BLOCK IS " + block.toString());
-			if (block.equals(Blocks.WATER)|| block.equals(Blocks.FLOWING_WATER)) {
-				double chance = probability;
-				
-				Biome biome = world.getBiomeForCoordsBody(pos);
-				if (biome.equals(Biomes.EXTREME_HILLS)) {
-					chance += bonus;
-				}
-				
-				if (true) {
-					player.inventory.addItemStackToInventory(new ItemStack(Items.GOLD_NUGGET));
-					if (!world.isRemote) {
-						System.out.println("spawning now...");
-						world.spawnEntityInWorld(new EntityXPOrb(world, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), 5));
-						System.out.println("done spawning.");
-					}
+			Biome biome = world.getBiomeForCoordsBody(pos);
+			if (biome.equals(Biomes.EXTREME_HILLS)) {
+				chance += bonus;
+			}
+			
+			if (true) {
+				player.inventory.addItemStackToInventory(new ItemStack(Items.GOLD_NUGGET));
+				if (!world.isRemote) {
+					System.out.println("spawning now...");
+					world.spawnEntityInWorld(new EntityXPOrb(world, (double)pos.getX(), (double)pos.getY(), (double)pos.getZ(), 5));
+					System.out.println("done spawning.");
 				}
 			}
-    		return EnumActionResult.SUCCESS;
-    	}
-    }
-    
-    private static BlockPos wheresTheWater(BlockPos blockPos, EnumFacing facing) {
-    	if (facing == EnumFacing.UP)
-    		return blockPos.up();
-    	if (facing == EnumFacing.DOWN)
-    		return blockPos.down();
-    	if (facing == EnumFacing.NORTH)
-    		return blockPos.north();
-    	if (facing == EnumFacing.SOUTH)
-    		return blockPos.south();
-    	if (facing == EnumFacing.WEST)
-    		return blockPos.west();
-    	if (facing == EnumFacing.EAST)
-    		return blockPos.east();
-    	
-    	return blockPos.up();
+		}
+		return EnumActionResult.SUCCESS;
     }
 }
